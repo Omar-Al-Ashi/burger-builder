4b8d4fa7007a1cab24419ec36b990c82fccf4be2
Omar <omar.ashi@hotmail.com>
2020-09-26 23:11:37 +0400

    Import Layout.css and apply class to main in Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import classes from "./Layout.css";
 
 /* eslint-disable jsx-a11y/href-no-hash */
 const layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.content}>{props.children}</main>
   </Aux>
 );
 
