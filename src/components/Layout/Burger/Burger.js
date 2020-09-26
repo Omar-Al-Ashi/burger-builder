08ecdb182554287c3dd050d51b0a178e61cc7adc
Omar <omar.ashi@hotmail.com>
2020-09-27 00:38:55 +0400

    Add Burger.css classes to represent burger and media queries

---
+++ src/components/Layout/Burger/Burger.js
@@ -0,0 +1,37 @@
+import React from "react";
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+import PropTypes from "prop-types";
+
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((ingredientsKey) => {
+      return [...Array(props.ingredients[ingredientsKey])].map((_, index) => {
+        return (
+          <BurgerIngredient
+            key={ingredientsKey + index}
+            type={ingredientsKey}
+          />
+        );
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+  return (
+    <div className={classes.burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
+
+burger.propTypes = {
+  ingredients: PropTypes.object.isRequired,
+};
